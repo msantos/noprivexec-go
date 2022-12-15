071a7e4ccd28c596aa612023843511bfb557af1b
Michael Santos <michael.santos@gmail.com>
2022-12-15 07:12:19 -0500

    noprivexec: disable setuid privileges

---
+++ cmd/noprivexec/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+
+	"codeberg.org/msantos/noprivexec"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("usage:", os.Args[0], "<cmd> <...>")
+		os.Exit(2)
+	}
+
+	if err := noprivexec.Enable(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	arg0, err := exec.LookPath(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(127)
+	}
+
+	if err := syscall.Exec(arg0, os.Args[1:], os.Environ()); err != nil {
+		fmt.Println(err)
+	}
+	os.Exit(126)
+}
