00728ef472dfe2bc23d43b670cad7d9f9031f368
Michael Santos <michael.santos@gmail.com>
2025-11-28 07:57:16 -0500

    Fix usage

--- cmd/noprivexec/main.go
+++ cmd/noprivexec/main.go
@@ -11,7 +11,7 @@ import (
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Fprintf(os.Stderr, "usage: %s <cmd> <...>", os.Args[0])
+		fmt.Fprintf(os.Stderr, "usage: %s <cmd> <...>\n", os.Args[0])
 		os.Exit(2)
 	}
 
